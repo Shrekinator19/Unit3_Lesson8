5849e0d466e332ac3e363ffefe201701e542e9e7
Shrekinator19 <43785257+Shrekinator19@users.noreply.github.com>
2018-12-11 10:10:27 -0500

    commit two.

---
+++ problem1.py
@@ -0,0 +1,7 @@
+Age = 28
+
+Year Born = (2018 - Age)
+
+Answer = (Year Born + 100)
+
+Print(Answer)
---
+++ problem2.py
@@ -0,0 +1,17 @@
+message = 'tjwz wz tjq zqurqt pqzzagq'
+
+# do not change the code above this line
+
+# Write your algorithm to decode the message here:
+
+message = 'tjwz wz tjq zqurqt pqzzagq'
+message.replace('q', 'e')
+print(message)
+
+
+
+
+
+# do not change the code below this line
+
+print(message)
