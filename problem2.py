7271f5b4aeed227d77eff48a9b77680d21e69853
Shrekinator19 <43785257+Shrekinator19@users.noreply.github.com>
2018-12-12 12:13:58 -0500

    commit one

---
+++ problem2.py
@@ -0,0 +1,34 @@
+message = 'tjwz wz tjq zqurqt pqzzagq'
+
+# do not change the code above this line
+
+# Write your algorithm to decode the message here:
+message = 'tjwz wz tjq zqurqt pqzzagq'
+message.replace('q','e' )
+print(message)
+
+message = 'tjwz wz tje zeuret pezzage'
+message.replace ('z','s' )
+print(message)
+
+message = 'tjws ws tje seuret pessage'
+message.replace ('p','m' )
+print(message)
+
+message = 'tjws ws tje seuret message'
+message.replace ('u','c' )
+print(message)
+
+message = 'tjws ws tje secret message'
+message.replace ('w','i' )
+print(message)
+
+message = 'tjis is tje secret message'
+message.replace ('j','h' )
+print(message)
+
+message = 'this is the secret message'
+print(message)
+# do not change the code below this line
+
+print(message)
